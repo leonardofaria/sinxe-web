120eab63a056567a2d889110b38d070fd4658fd8
Andy Tung <andyytung@gmail.com>
2016-09-24 16:12:17 -0700

    added a input of catagories

--- app/choices/route.js
+++ app/choices/route.js
@@ -18,6 +18,12 @@ export default Ember.Route.extend({
     });
 
   },
+  setupController: function(controller, model) {
+    var categories = ["Afghan", "African", "American (New)", "American (Traditional)", "Arabian", "Argentine", "Armenian", "Asian Fusion", "Australian", "Austrian", "Bangladeshi", "Barbeque", "Basque", "Belgian", "Brasseries", "Brazilian", "Breakfast & Brunch", "British", "Buffets", "Burgers", "Burmese", "Cafes", "Cafeteria", "Cajun/Creole", "Calabrian", "Cambodian", "Cantonese", "Caribbean", "Catalan", "Cheesesteaks", "Chicken Shop", "Chicken Wings", "Chinese", "Colombian", "Comfort Food", "Creperies", "Cuban", "Czech", "Delis", "Dim Sum", "Diners", "Dinner Theater", "Dominican", "Egyptian", "Ethiopian", "Falafel", "Fast Food", "Filipino", "Fish & Chips", "Fondue", "Food Court", "Food Stands", "French", "Gastropubs", "German", "Gluten-Free", "Greek", "Hainan", "Haitian", "Halal", "Hawaiian", "Himalayan/Nepalese", "Hong Kong Style Cafe", "Hot Dogs", "Hot Pot", "Hungarian", "Iberian", "Indian", "Indonesian", "Irish", "Italian", "Izakaya", "Japanese", "Korean", "Kosher", "Laotian", "Latin American", "Lebanese", "Live/Raw Food", "Malaysian", "Mediterranean", "Mexican", "Middle Eastern", "Modern European", "Mongolian", "Moroccan", "New Mexican Cuisine", "Nicaraguan", "Noodles", "Pakistani", "Persian/Iranian", "Peruvian", "Pizza", "Polish", "Pop-Up Restaurants", "Portuguese", "Poutineries", "Puerto Rican", "Ramen", "Russian", "Salad", "Salvadoran", "Sandwiches", "Sardinian", "Scandinavian", "Scottish", "Seafood", "Senegalese", "Shanghainese", "Singaporean", "Slovakian", "Soul Food", "Soup", "South African", "Southern", "Spanish", "Sri Lankan", "Steakhouses", "Supper Clubs", "Sushi Bars", "Syrian", "Szechuan", "Taiwanese", "Tapas Bars", "Tapas/Small Plates", "Teppanyaki", "Tex-Mex (te", "Thai", "Themed Cafes", "Trinidadian", "Turkish", "Tuscan", "Ukrainian", "Uzbek", "Vegan", "Vegetarian", "Venezuelan", "Vietnamese", "Waffles"];
+
+    controller.set('categories', categories);
+    controller.set('model', model);
+  },
   actions: {
     selectChoice(itemID) {
       const currentUser = this.get('session').get('currentUser');
