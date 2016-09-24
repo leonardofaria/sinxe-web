039d259a518a67e440c2f580894e9e3671e8c2d3
Leonardo Faria <leonardofaria@gmail.com>
2016-09-24 00:21:54 -0700

    Add logo; Update layout; Add profile route

--- app/profile/controller.js
+++ app/profile/controller.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    updateUser: function() {
+      this.get('model').save();
+      this.transitionToRoute('choices');
+    }
+  }
 });
