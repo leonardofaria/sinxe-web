61006cc0b9e973d276d9bde22e938951404d274e
Colby Song <colbysong@Colbys-iMac.local>
2016-09-23 23:55:43 -0700

    added selectChoice functionality

--- app/choices/route.js
+++ app/choices/route.js
@@ -3,6 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('event');
+  },
+  actions: {
+    selectChoice(itemID) {
+      var users = this.store.findAll('user');
+      const currentUser = this.get('session').get('currentUser');
+      console.log(currentUser.uid);
+      const user = users.filter(function (el) {
+        return el.get('uid') === currentUser.uid;
+      });
+      console.log(user);
+      console.log(itemID);
+    }
   }
 
 });
