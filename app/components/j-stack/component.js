9df19deedf24680e2431b19586f0545adee6e17f
Colby Song <colbysong@Colbys-iMac.local>
2016-09-23 23:18:12 -0700

    yea

--- app/components/j-stack/component.js
+++ app/components/j-stack/component.js
@@ -3,10 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement: function() {
     let items = this.get('items');
-    console.table(items);
+    console.log(items);
 
     items.forEach((item) => {
-      let membersInfo = item[3];
+      console.log(item);
+      let membersInfo = item.data.users;
       let members = '';
 
       if (Array.isArray(membersInfo)) {
@@ -18,11 +19,11 @@ export default Ember.Component.extend({
       }
 
       let output = `
-        <li>
-          <div class="img" style="background-image: url(${item[2]})"></div>
+        <li data-id="${item.id}" >
+          <div class="img" style="background-image: url(${item.data.imageUrl})"></div>
 
           <div class="event">
-            <h2>${item[0]}</h2>
+            <h2>${item.data.restaurantName}</h2>
             <div class="members">
               ${members}
             </div>
@@ -30,7 +31,7 @@ export default Ember.Component.extend({
 
           <div class="event-details">
             <div class="description">
-              ${item[1]}
+              ${item.data.address}
             </div>
             <div class="quantity">
               <span class="fa fa-users"></span> <span>${item[4]}</span>
@@ -52,6 +53,8 @@ export default Ember.Component.extend({
       },
       // like callback
       onLike: function (item) {
+        console.log($(item).data('id'));
+
         // set the status text
         Ember.$('#status').html('Like image ' + (item.index()+1));
       },
