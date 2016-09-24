f45f41f95adf5e52f16758926f0a7b1aca242321
Leonardo Faria <leonardofaria@gmail.com>
2016-09-23 20:23:08 -0700

    Add User model

---
+++ app/user/model.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  uid: DS.attr('string'),
+  name: DS.attr('string'),
+  email: DS.attr('string'),
+  photoUrl: DS.attr('string'),
+  bio: DS.attr('string'),
+  occupation: DS.attr('number')
+});
