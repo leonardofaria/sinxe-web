f2f78893153ef8b82740abbc9d2c4571c82b8941
Leonardo Faria <leonardofaria@gmail.com>
2016-09-25 08:52:49 -0700

    Update user profile page

--- app/router.js
+++ app/router.js
@@ -15,7 +15,7 @@ Router.map(function() {
   this.route('create-event');
   this.route('chats', {path : "/chats/:chat_id"});
 
-  this.route('biography', {path : "/biography/:user_id"});
+  this.route('biography', {path : "/users/:user_id"});
 });
 
 export default Router;
