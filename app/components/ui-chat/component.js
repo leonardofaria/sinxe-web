b80db9271c4672ea6e445aedd787a7ca919386a5
Leonardo Faria <leonardofaria@gmail.com>
2016-09-24 21:49:23 -0700

    Fix timestamp conversion in chat

--- app/components/ui-chat/component.js
+++ app/components/ui-chat/component.js
@@ -12,14 +12,31 @@ export default Ember.Component.extend({
     Ember.$('html, body').animate({ scrollTop: Ember.$("#messages li:last-of-type").offset().top }, 450);
 
     Ember.$('time').each(function() {
-      let timestamp = $(this).find('span').text();
-      // let date ()
-
-      console.log(timestamp);
+      let timestamp = $(this).find('span').data('timestamp');
       let date = $.timeago(new Date(timestamp * 1000));
       Ember.$(this).find('span').text(date);
-      console.log(date);
     });
+
+    var chatHeight = Ember.$('#messages').height();
+
+    var updateMessages = setInterval(function() {
+      var messages = Ember.$('#messages');
+
+      if (messages.length > 0) {
+        if (chatHeight !== Ember.$('#messages').height()) {
+          Ember.$('html, body').animate({ scrollTop: Ember.$("#messages li:last-of-type").offset().top }, 450);
+          chatHeight = Ember.$('#messages').height();
+        }
+
+        Ember.$('time').each(function() {
+          let timestamp = $(this).find('span').data('timestamp');
+          let date = $.timeago(new Date(timestamp * 1000));
+          Ember.$(this).find('span').text(date);
+        });
+      } else {
+        clearInterval(updateMessages);
+      }
+    }, 1000);
   }
 
 });
