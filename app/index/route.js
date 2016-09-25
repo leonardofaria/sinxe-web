bd607b84ab45efe90f6bf18a290e223c9a655894
Leonardo Faria <leonardofaria@gmail.com>
2016-09-25 07:48:52 -0700

    Fix index redirection

--- app/index/route.js
+++ app/index/route.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    let currentUser = this.get('session').get('currentUser');
+
+    if (currentUser !== undefined) {
+      Ember.$('body').removeClass('index');
+      this.transitionTo('choices');
+    }
+  }
 });
